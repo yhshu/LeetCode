5590fde248dcae30f9ccd1221e61a6e6803da819
Yiheng Shu <lzw429@qq.com>
2017-09-04 22:53:46 +0800

    update

--- 67. Add Binary.cpp
+++ 67. Add Binary.cpp
@@ -6,58 +6,14 @@ a = "11"
 b = "1"
 Return "100".
  */
-
+//https://fisherlei.blogspot.com/2013/01/leetcode-add-binary.html
 class Solution {
 public:
-    string XOR(string a, string b, int len) {
-        string res = "";
-        for (int i = len - 1; i != -1; --i) {
-            if ((a[i] == '1' && b[i] == '0') || (a[i] == '0' && b[i] == '1'))
-                res[i] = '1';
-            else res[i] = '0';
-        }
-        return res;
-    }
-
-    string AND(string a, string b, int len) {
-        string res = "";
-        for (int i = len - 1; i != -1; --i) {
-            if (a[i] == '1' && b[i] == '1')
-                res[i] = '1';
-            else res[i] = '0';
-        }
-        return res;
-    }
-
-    bool isZeros(string str) {
-        for (int i = 0; i != str.length(); ++i)
-            if (str[i] != '0')return false;
-        return true;
-    }
-
-    string LeftShiftOne(string str, int len) {
-        string res = "";
-        for (int i = 0; i != len - 1; ++i) {
-            res[i] = str[i + 1];
-        }
-        res[len] = '0';
-        return res;
-    }
-
-    string ADD(string a, string b, int len) {
-        string carry = AND(a, b, len);
-        if (isZeros(carry))
-            return XOR(a, b, len);
-        else return ADD(XOR(a, b, len), LeftShiftOne(AND(a, b, len), len), len);
-    }
-
     string addBinary(string a, string b) {
-        int len = max(a.length(), b.length());
-        //将两数位对齐
-        while (a.length() < len)
-            a = "0" + a;
-        while (b.length() < len)
-            b = "0" + b;
-        return ADD(a, b, len);
+        int carry = 0;
+        string res;
+        for (int i = a.size() - 1, j = b.size() - 1; i != -1 || j != -1; --i, --j) {
+
+        }
     }
 };
