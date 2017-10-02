a737ef3928c51aa30992cb7338e736a376a1e83f
Yiheng Shu <lzw429@qq.com>
2017-10-02 22:46:21 +0800

    update

---
+++ 225. Implement Stack using Queues.cpp
@@ -0,0 +1,58 @@
+/*
+Implement the following operations of a stack using queues.
+
+push(x) -- Push element x onto stack.
+pop() -- Removes the element on top of the stack.
+top() -- Get the top element.
+empty() -- Return whether the stack is empty.
+Notes:
+You must use only standard operations of a queue -- which means only push to back, peek/pop from front, size, and is empty operations are valid.
+Depending on your language, queue may not be supported natively. You may simulate a queue by using a list or deque (double-ended queue), as long as you use only standard operations of a queue.
+You may assume that all operations are valid (for example, no pop or top operations will be called on an empty stack).
+ */
+
+class MyStack {
+public:
+    /** Initialize your data structure here. */
+    MyStack() {
+
+    }
+
+    /** Push element x onto stack. */
+    void push(int x) {
+        que.push(x);
+        for (auto i = 0; i != que.size() - 1; ++i) {
+            que.push(que.front());
+            que.pop();
+        }
+    }
+
+    /** Removes the element on top of the stack and returns that element. */
+    int pop() {
+        int res = que.front();
+        que.pop();
+        return res;
+    }
+
+    /** Get the top element. */
+    int top() {
+        return que.front();
+    }
+
+    /** Returns whether the stack is empty. */
+    bool empty() {
+        return que.empty();
+    }
+
+private:
+    queue<int> que;
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * bool param_4 = obj.empty();
+ */
---
+++ 232. Implement Queue using Stacks.cpp
@@ -0,0 +1,68 @@
+/*
+Implement the following operations of a queue using stacks.
+
+push(x) -- Push element x to the back of queue.
+pop() -- Removes the element from in front of queue.
+peek() -- Get the front element.
+empty() -- Return whether the queue is empty.
+Notes:
+You must use only standard operations of a stack -- which means only push to top, peek/pop from top, size, and is empty operations are valid.
+Depending on your language, stack may not be supported natively. You may simulate a stack by using a list or deque (double-ended queue), as long as you use only standard operations of a stack.
+You may assume that all operations are valid (for example, no pop or peek operations will be called on an empty queue).
+ */
+
+class MyQueue {
+public:
+    /** Initialize your data structure here. */
+    MyQueue() {
+
+    }
+
+    /** Push element x to the back of queue. */
+    void push(int x) {
+        if (!s.size()) {
+            s.push(x);
+            return;
+        }
+        stack<int> s1;
+        while (s.size() > 0) {
+            s1.push(s.top());
+            s.pop();
+        }
+        s.push(x);
+        while (s1.size() > 0) {
+            s.push(s1.top());
+            s1.pop();
+        }
+
+    }
+
+    /** Removes the element from in front of queue and returns that element. */
+    int pop() {
+        int ret = s.top();
+        s.pop();
+        return ret;
+    }
+
+    /** Get the front element. */
+    int peek() {
+        return s.top();
+    }
+
+    /** Returns whether the queue is empty. */
+    bool empty() {
+        return s.empty();
+    }
+
+private:
+    stack<int> s;
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * bool param_4 = obj.empty();
+ */
