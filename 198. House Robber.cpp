47266de65d2c44ff7e8b523e2a3f9c834599fc6e
Yiheng Shu <lzw429@qq.com>
2017-10-29 22:07:48 +0800

    update

---
+++ 198. House Robber.cpp
@@ -0,0 +1,23 @@
+/*
+ * You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security system connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount of money you can rob tonight without alerting the police.
+ */
+
+class Solution {
+public:
+	int rob(vector<int>& nums) {
+		int a = 0, b = 0;
+		for (int i = 0; i != nums.size(); ++i)
+		{
+			if (i % 2 == 0)
+			{
+				a = max(nums[i] + a, b);
+
+			}
+			else
+				b = max(nums[i] + b, a);
+		}
+		return max(a, b);
+	}
+};
---
+++ 695. Max Area of Island.cpp
@@ -0,0 +1,46 @@
+/*
+ * Given a non-empty 2D array grid of 0's and 1's, an island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.) You may assume all four edges of the grid are surrounded by water.
+
+Find the maximum area of an island in the given 2D array. (If there is no island, the maximum area is 0.)
+
+Example 1:
+[[0,0,1,0,0,0,0,1,0,0,0,0,0],
+ [0,0,0,0,0,0,0,1,1,1,0,0,0],
+ [0,1,1,0,1,0,0,0,0,0,0,0,0],
+ [0,1,0,0,1,1,0,0,1,0,1,0,0],
+ [0,1,0,0,1,1,0,0,1,1,1,0,0],
+ [0,0,0,0,0,0,0,0,0,0,1,0,0],
+ [0,0,0,0,0,0,0,1,1,1,0,0,0],
+ [0,0,0,0,0,0,0,1,1,0,0,0,0]]
+Given the above grid, return 6. Note the answer is not 11, because the island must be connected 4-directionally.
+Example 2:
+[[0,0,0,0,0,0,0,0]]
+Given the above grid, return 0.
+Note: The length of each dimension in the given grid does not exceed 50.
+ */
+
+class Solution {
+public:
+	int maxAreaOfIsland(vector<vector<int>>& grid) {
+		int res = 0;
+		for (int i = 0; i != grid.size(); ++i)
+		{
+			for (int j = 0; j != grid[0].size(); ++j)
+			{
+				if (grid[i][j] == 1)
+					res = max(res, AreaOfIsland(grid, i, j));
+			}
+		}
+		return res;
+	}
+	// DFS
+	int AreaOfIsland(vector<vector<int>>& grid, int i, int j)//计算(i,j)所在岛屿的面积
+	{
+		if (i >= 0 && i < grid.size() && j >= 0 && j < grid[0].size() && grid[i][j] == 1)//当前是在岛屿
+		{
+			grid[i][j] = 0;//避免重复计算
+			return 1 + AreaOfIsland(grid, i + 1, j) + AreaOfIsland(grid, i - 1, j) + AreaOfIsland(grid, i, j + 1) + AreaOfIsland(grid, i, j - 1);
+		}
+		return 0;
+	}
+};
