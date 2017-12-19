2ce9d7de03b504c5275d09f25239fa5fe92a91ae
Yiheng Shu <lzw429@qq.com>
2017-12-19 15:12:27 +0800

    update

--- Attempted/83. Remove Duplicates from Sorted List.cpp
+++ Attempted/83. Remove Duplicates from Sorted List.cpp
@@ -16,13 +16,15 @@ Given 1->1->2->3->3, return 1->2->3.
 */
 class Solution {
 public:
-    ListNode *deleteDuplicates(ListNode *head) {
-        if (!head)
-            return NULL;
-        ListNode *res = head;
-        while (res->next) {
-
-            res = res->next;
-        }
-    }
+	ListNode *deleteDuplicates(ListNode *head) {
+		if (!head || !head->next)return head;// 考虑head的长度为0或1
+		ListNode *node = head;
+		while (node != nullptr && node->next != nullptr) // 判断node->next是否为空前，先判断node是否为空
+		{
+			if (node->val == node->next->val) // 如果node与node->next相等，node位置不改变，仅修改node->next
+				node->next = node->next->next;
+			else node = node->next;
+		}
+		return head;
+	}
 };
